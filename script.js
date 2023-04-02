c1ae5e61715de11c73875a931ce72c87db02461e
Bekam <mspbekan@gmail.com>
2023-04-01 19:51:56 -0500

    elements created with js, some styling added, and functions refined

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 //Declare an array with several objects.....
 var questionsPack = [
     {
-        question: "My first question?",
-        choices: ["choice  A", "choice B", "choice C", "choice D"],
-        answer: "choice B",
+        question: "Javascript is an _____ language?",
+        choices: ["Obejct-oriented", "Procedural", "Object-based", "All"],
+        answer: "Object-oriented",
     },
     {
-        question: "My second question?",
-        choices: ["choice  A", "choice B", "choice C", "choice D"],
-        answer: "choice B",
+        question: "Which of the following methods can be used to display data in some form using Javascript?",
+        choices: ["document.write()", "console.log()", "window.alert()", "All"],
+        answer: "All",
     },
     {
         question: "My third question?",
@@ -38,13 +38,14 @@ var questionsPack = [
 - so on until the last question or the timer ===0
 -show total score
 -ask for initials and submit
+-store the initials and score values
 -initials and highscore will be displayed with 'Go back' and 'clear highscore' options
-- when 'Go back' gets clicked, prompts user to click the start button
+- when 'Go back' gets clicked, reload page
 
 */
 
 //Declare and initialize variables
-var givenTime = 15;
+var givenTime = 30;
 var totalScore = 0;
 var thisQuestion = 0;
 // var multipleChoices = 0;
@@ -52,7 +53,6 @@ var quizDivEl = document.getElementById("quiz-div");
 var instructionDivEl = document.getElementById("instruction-div")
 var timer = document.getElementById("timer");
 var startButton = document.getElementById("startBtn");
-// var stopCommand = document.getElementById("stop");
 var questionEl = document.getElementById("question");
 var choicesOlEl = document.getElementById("choices");
 var CorrectAnswer = document.querySelector("#correct-answer");
@@ -61,26 +61,16 @@ var scoreDivEl = document.getElementById("highscore");
 var submitButton = document.getElementById("submitFinal");
 var scorePEl = document.getElementById("score-paragraph");
 var userInitials = document.querySelector('input[name="user-initials"]');
-var initialAndHighscore = document.getElementById("initialAndScore");
-var goBackButton = document.getElementById("goBackBtn");
-var clearButton = document.getElementById("clearHighscore");
-
-
-
 
 
 startButton.addEventListener("click", startTimer)
 startButton.addEventListener("click", displayQuestions);
 
-//create elements, rather than using the html, for the "#summary" and "#highscore" to appear only whenever get called one after the other 
 
-//Create class and style the border of the div
-quizDivEl.classList.add("hide-div");
-quizDivEl.setAttribute("style", "border-bottom-style: solid");
 
-//Create an anchor element for multiple choices and style them
+
+//Create an anchor element for multiple choices and style them ..........????
 var listOfChoices;
-// var choiceWrapButton = document.createElement("button");
 listOfChoices.setAttribute("style", "border:solid; background-color:purple")
 
 
@@ -89,36 +79,24 @@ function startTimer() {
     var timeInterval = setInterval(function () {
         givenTime--;
         timer.textContent = "Time:" + givenTime;
-        // instructionDivEl.classList.add("newInstruction-div");
-        // instructionDivEl.remove();
 
+            //Create class and style the border of the div
+            quizDivEl.classList.add("hide-div");
+            quizDivEl.setAttribute("style", "border-bottom-style: solid");
 
         if (givenTime <= 0 || thisQuestion >= questionsPack.length) {
             clearInterval(timeInterval);
-            // timer.textContent ="";
-            // questionEl.textContent = "";
-            // choicesOlEl.innerHTML = "";
-            // startButton.classList.add("hide-btn");
-
             quizDivEl.remove();
             timer.remove();
-            // startButton.remove();
-
             submit();
         }
 
     }, 1000)
 
     displayQuestions();
-
-    //use setTimeOut();
 };
 
-
-
-
 function displayQuestions() {
-
     questionEl.textContent = questionsPack[thisQuestion].question;
     choicesOlEl.innerHTML = "";
 
@@ -127,12 +105,15 @@ function displayQuestions() {
 
         listOfChoices.innerHTML = questionsPack[thisQuestion].choices[i];
         choicesOlEl.appendChild(listOfChoices);
-        // choicesOlEl.appendChild(choiceWrapButton);
 
 
-        listOfChoices.setAttribute("style", "border:solid; background-color: rgb(197, 142, 248);; width: fit-content; font-weight: bold; padding: 1%; margin: 1%; border-radius: 10%")
+        listOfChoices.setAttribute("style", "border:solid; background-color: rgb(197, 142, 248);; width: fit-content; font-weight: bold; font-size: 1rem; padding: 1%; margin: 1%; border-radius: 10%");
+
+        questionEl.setAttribute("style", "font-weight: bold; font-size: 1rem");
+
+
+        //add event listener
         listOfChoices.addEventListener("click", function () {
-
             choicesOlEl.textContent = "";
             questionEl.textContent = ""
             thisQuestion++;
@@ -144,62 +125,71 @@ function displayQuestions() {
                 CorrectAnswer.textContent = "Wrong";
                 givenTime -= 5;
             }
-            if (checkAnswer !== questionsPack[thisQuestion].answer && givenTime >=6) {
+            if (checkAnswer !== questionsPack[thisQuestion].answer && givenTime >=5) {
                 var timeOut = setTimeout(function () {
                     givenTime;
 
                 }, 2000)
                 clearTimeout(timeOut);
             }
-
-
-
-
         })
+        
     }
-
-
 }
-
-
 
 
 function submit() {
-    // questionEl.innerHTML = "";
-    // choicesOlEl.innerHTML = "";
-    // timer.textContent = "";
 
-    userInitials.value = "";
+    //.. to hold the user initials and score
+    summaryDivEl.setAttribute("style", "display: block; margin-left: 35%");
     var allDonePEl = document.getElementById("allDone");
     allDonePEl.textContent = "All Done!";
-    // scorePEl.textContent = 
-    // initialAndHighscore.appendChild(scorePEl);
 
-    // var labelEl = document.querySelector("label");
-var labelEl =document.createElement("label");
-    labelEl.textContent = "Your final score: " + totalScore;
-    scorePEl.appendChild(labelEl);
-
-   
-
+    scorePEl.textContent = "Your Score is: " + totalScore;  //totalScore not increment
+    
+    // to store the user's initials and total score values
     submitButton.addEventListener("click", function () {
         localStorage.setItem("userInitials", JSON.stringify(userInitials.value))
         localStorage.setItem("userScore", JSON.stringify(totalScore))                   //is there any way to concatenate?
-        userInitials.value = "";
+        summaryDivEl.remove();
         displayHighscore();
     })
+    
 }
 
 function displayHighscore() {
-    initialAndHighscore.textContent += localStorage.getItem("userScore") + "-" + localStorage.getItem("userInitials");
-    //is there any way to concatenate?
+
+    //create a paragraph element to hold and display the initials and score again
+
+    var initialAndHighscore = document.createElement("p");
+    
+    // initialAndHighscore.textContent += localStorage.getItem("userScore") + "-" + localStorage.getItem("userInitials");
+    initialAndHighscore.textContent = "1." + userInitials.value + ": " + totalScore;  //seems unprofessional to put the list of number manually, any other way?  totalScore not increment
+    scoreDivEl.appendChild(initialAndHighscore);
+
+    //Create button elements
+
+    var goBackBtn = document.createElement("button");
+    goBackBtn.textContent = "Go Back";
+    scoreDivEl.appendChild(goBackBtn);
+
+    var clearButton = document.createElement("button");
+    clearButton.textContent = "Clear Highscore";
+    scoreDivEl.appendChild(clearButton);
+
+    //style the div
+    scoreDivEl.setAttribute("style", "display: block; width: 100%; margin-left: 35%");
+    //style the buttons
+    clearButton.setAttribute("style", "border:solid; background-color: rgb(197, 142, 248);; width: fit-content; font-weight: bold; padding: 1%; margin: 1%; border-radius: 10%");
+
+    goBackBtn.setAttribute("style", "border:solid; background-color: rgb(197, 142, 248);; width: fin-content; font-weight: bold; padding: 1%; margin: 1%; border-radius: 10%");
+
 
     clearButton.addEventListener("click", function () {
         initialAndHighscore.textContent = "";
     })
-    goBackButton.addEventListener("click", function () {
+    goBackBtn.addEventListener("click", function () {
 
-        ////////???????? i need to get back to the home page
         location.reload();
     })
 }
